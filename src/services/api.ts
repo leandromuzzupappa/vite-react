179ae2f397a9ffadd1d7a5b91cb9d2fb0808f7e5
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2022-09-05 17:27:01 -0300

    service

---
+++ src/services/api.ts
@@ -0,0 +1,16 @@
+let controller: any;
+
+const ENDPOINT = "https://rickandmortyapi.com/api/character/?name=";
+
+export const fetchCharacters = (query: string) => {
+  if (controller) controller.abort();
+
+  controller = new AbortController();
+
+  return fetch(ENDPOINT + query, { signal: controller.signal })
+    .then((response) => response.json())
+    .then((charactersList) => {
+      controller = null;
+      return charactersList.results;
+    });
+};
